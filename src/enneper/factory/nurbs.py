4ebd56f3fe2b3272107edf8f90b7ee22ce9e9489
Andreas Kührmann <andreas.kuehrmann@gmail.com>
2012-12-05 19:35:35 +0100

    FIXED: get_surface_of_revolution function

--- src/enneper/factory/nurbs.py
+++ src/enneper/factory/nurbs.py
@@ -25,7 +25,10 @@ import numpy as np
 
 import enneper.curves as ec
 import enneper.surfaces as es
-import enneper.foundation.geometry as efg
+import enneper.foundation as fdn
+
+
+npa = lambda array: np.array(array)
 
 
 def get_ruled_surface(curves):
@@ -33,46 +36,53 @@ def get_ruled_surface(curves):
     degree_u, degree_v = max(curves[0].degree, curves[1].degree), 1
     map(lambda curve: curve.elevate_degree(degree_u - curve.degree), curves)
     map(lambda i: curves[i % 2].merge_knots(curves[(i + 1) % 2].knots), [0, 1])
-    knots_u, knots_v = curves[0].knots, np.array([0., 0., 1., 1.])
+    knots_u, knots_v = curves[0].knots, npa([0., 0., 1., 1.])
     ctrl_points = np.append(*[[curve.ctrl_points] for curve in curves])
     return es.NURBSSurface(ctrl_points, degree_u, degree_v, knots_u, knots_v)
 
 
-def get_surface_of_revolution(position_vector, direction_vector, curve, theta):
-    if 2 * np.pi < theta < 0:
-        raise ValueError
-    narcs = 4
-    narcs = 3 if theta < 1.5 * np.pi else narcs
-    narcs = 2 if theta < np.pi else narcs
-    narcs = 1 if theta < 0.5 * np.pi else narcs
-    dtheta = theta / narcs
-    surface = es.NURBSSurface()
-    if narcs == 2:
-        surface.knots_u[3] = surface.knots_u[4] = 0
-    if narcs == 3:
-        surface.knots_u[3] = surface.knots_u[4] = 1 / 3.
-        surface.knots_u[5] = surface.knots_u[6] = 2 / 3.
-    if narcs == 4:
-        surface.knots_u[3] = surface.knots_u[4] = .25
-        surface.knots_u[5] = surface.knots_u[6] = .5
-        surface.knots_u[7] = surface.knots_u[8] = .75
-    surface.knots_u[:3] = np.zeros(3)
-    surface.knots_u[(1 + 2 * narcs):(4 + 2 * narcs)] = np.ones(3)
-    surface.knots_v[:] = curve.konts
-    wm = np.cos(dtheta / 2)
-    cosines = np.cos(dtheta * np.arange(1, narcs + 1))
-    sines = np.sin(dtheta * np.arange(1, narcs + 1))
-    for j, ctrl_point in enumerate(curve.ctrl_points):
-        o = efg.project_to_line(position_vector, direction_vector, ctrl_point)
-        x = ctrl_point - o
-        y = np.cross(direction_vector, x)
-        r = np.sqrt(np.dot(x, x))
-        p0, t0 = surface.ctrl_points[0, j], _ = curve.ctrl_points[j], y
-        for i in range(1, narcs):
+def get_surface_of_revolution(curve, pos_v, dir_v, phi):
+    narcs = 3 if phi < 1.5 * np.pi else 4
+    narcs = 2 if phi < np.pi else narcs
+    narcs = 1 if phi < .5 * np.pi else narcs
+    n = 2 * narcs + 1
+    m = curve.ctrl_points.shape[0]
+    dphi = phi / float(narcs)
+    cosines = np.cos(dphi * np.arange(narcs + 1))
+    sines = np.sin(dphi * np.arange(narcs + 1))
+    wm = np.cos(.5 * dphi)
+    pj = fdn.nurbs.get_cartesian_points(curve.ctrl_points)
+    wj = curve.ctrl_points[:, -1]
+    surface = es.NURBSSurface((n, m, 4), 2, curve.degree)
+    for i in range(1, narcs):
+        surface.knots_u[i * 2 + 1] = i / float(narcs)
+        surface.knots_u[i * 2 + 2] = i / float(narcs)
+    surface.knots_u[n:(n + 3)] = np.ones(3)
+    surface.knots_v[:] = curve.knots
+    for j in range(m):
+        o = fdn.math.project_point_to_line(pj[j], pos_v, dir_v)
+        x = pj[j] - o
+        y = np.cross(dir_v, x)
+        r = fdn.math.p_norm(x, 2)
+        index = 0
+        if r < 1e-7:
+            h_point = fdn.nurbs.get_homogeneous_points(o, wj[j])
+            for i in range(narcs * 2 + 1):
+                surface.ctrl_points[i, j] = h_point
+            continue
+        h_point = fdn.nurbs.get_homogeneous_points(pj[j], wj[j])
+        surface.ctrl_points[0, j] = h_point
+        p0 = pj[j]
+        t0 = y
+        for i in range(1, narcs + 1):
             p2 = o + r * cosines[i] * x + r * sines[i] * y
             t2 = -sines[i] * x + cosines[i] * y
-            pij = efg.intersect_line(p0, t0, p2, t2)
-            wij = ctrl_point[j, -1] * wm
-            surface.ctrl_points[i * 2 + 2, j] = p2, ctrl_point[j, -1]
-            surface.ctrl_points[i * 2 + 1, j] = pij, wij
-            p0, t0 = p2, t2 if i < narcs else p0, t0
+            h_point = fdn.nurbs.get_homogeneous_points(p2, wj[j])
+            surface.ctrl_points[index + 2, j] = h_point
+            pnt, _ = fdn.math.intersect_lines(npa([p0, p2]), npa([t0, t2]))
+            h_point = fdn.nurbs.get_homogeneous_points(pnt, wm * wj[j])
+            surface.ctrl_points[index + 1, j] = h_point
+            index += 2
+            if i < narcs:
+                p0, t0 = p2, t2
+    return surface
