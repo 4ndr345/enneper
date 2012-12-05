35e023fb5276066aabc15e4fc768658c79466006
Andreas Kührmann <andreas.kuehrmann@gmail.com>
2012-12-05 18:08:42 +0100

    ADDED: test methods for get_homogeneous_points and get_cartesian_points

--- src/testsuite/foundation.py
+++ src/testsuite/foundation.py
@@ -25,7 +25,7 @@ import unittest
 
 import numpy as np
 
-import enneper.foundation as ef
+import enneper.foundation as fdn
 
 
 class TestFoundation(unittest.TestCase):
@@ -33,13 +33,55 @@ class TestFoundation(unittest.TestCase):
     def setUp(self):
         self.degree = 2
         self.knots = np.array([0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5])
+        self.weights = np.array([1, .7, 1, .7, 1, .7, 1, .7, 1])
+        self.pnts = np.array([[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0],
+                              [-1, -1], [0, -1], [1, -1], [1, 0]])
 
     def test_find_span(self):
         """The NURBS Book 2nd edition: example 2.4 (page 71)"""
-        actual = ef.nurbs.find_span(2.5, self.degree, self.knots)
+        actual = fdn.nurbs.find_span(2.5, self.degree, self.knots)
         np.testing.assert_equal(actual, 4)
 
     def test_get_basis_functions(self):
         """The NURBS Book 2nd edition: example 2.4 (page 71)"""
-        actual = ef.nurbs.get_basis_functions(4, 2.5, self.degree, self.knots)
+        actual = fdn.nurbs.get_basis_functions(4, 2.5, self.degree, self.knots)
         np.testing.assert_equal(actual, np.array([0.125, 0.75, 0.125]))
+
+    def test_get_homogeneous_points_of_a_point(self):
+        pnts = np.array([1, 1])
+        weights = np.array([2])
+        actual = fdn.nurbs.get_homogeneous_point(pnts, weights)
+        desired = np.array([2, 2, 2])
+        np.testing.assert_equal(actual, desired)
+
+    def test_get_homogeneous_points_of_a_curve(self):
+        pnts = np.array([[1, 0], [1, 1]])
+        weights = np.array([1, 2])
+        actual = fdn.nurbs.get_homogeneous_point(pnts, weights)
+        desired = np.array([[1, 0, 1], [2, 2, 2]])
+        np.testing.assert_equal(actual, desired)
+
+    def test_get_homogeneous_points_of_a_surface(self):
+        pnts = np.array([[[1, 0], [1, 1]], [[0, 1], [-1, 1]]])
+        weights = np.array([[1, 2], [1, 2]])
+        actual = fdn.nurbs.get_homogeneous_point(pnts, weights)
+        desired = np.array([[[1, 0, 1], [2, 2, 2]], [[0, 1, 1], [-2, 2, 2]]])
+        np.testing.assert_equal(actual, desired)
+
+    def test_get_cartesian_points_of_a_point(self):
+        ctrl_pnts = np.array([2, 2, 2])
+        actual = fdn.nurbs.get_cartesian_points(ctrl_pnts)
+        desired = np.array([1, 1])
+        np.testing.assert_equal(actual, desired)
+
+    def test_get_cartesian_points_of_a_curve(self):
+        ctrl_pnts = np.array([[1, 0, 1], [2, 2, 2]])
+        actual = fdn.nurbs.get_cartesian_points(ctrl_pnts)
+        desired = np.array([[1, 0], [1, 1]])
+        np.testing.assert_equal(actual, desired)
+
+    def test_get_cartesian_points_of_a_surface(self):
+        ctrl_pnts = np.array([[[1, 0, 1], [2, 2, 2]], [[0, 1, 1], [-2, 2, 2]]])
+        actual = fdn.nurbs.get_cartesian_points(ctrl_pnts)
+        desired = np.array([[[1, 0], [1, 1]], [[0, 1], [-1, 1]]])
+        np.testing.assert_equal(actual, desired)
