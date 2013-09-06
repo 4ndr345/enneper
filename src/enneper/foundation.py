c2401e952ecf868d5bf81c72a36e4647c97cb16f
Andreas Kührmann <andreas.kuehrmann@gmail.com>
2013-09-06 12:45:48 +0200

    CHANGED: merged foundation/nurbs.py and foundation/math.py into foundation.py

--- src/enneper/foundation.py
+++ src/enneper/foundation.py
@@ -24,6 +24,50 @@
 import numpy as np
 
 
+def find_span(u, degree, knots):
+    """Determine the knot span index"""
+    low, high = degree, knots.size - degree
+    # Special case
+    if u == knots[high - 1]:
+        return high - 2
+    # search
+    return np.where(knots[low:high] <= u)[0][-1] + degree
+
+
+def get_basis_functions(i, u, degree, knots):
+    """ Computes the nonvanishing basis functions"""
+    basis_func = np.ones(degree + 1)
+    left = np.ones(degree + 1)
+    right = np.ones(degree + 1)
+    for j in range(1, degree + 1):
+        left[j] = u - knots[i + 1 - j]
+        right[j] = knots[i + j] - u
+        cache = 0
+        for k in range(j):
+            tmp = basis_func[k] / (right[k + 1] + left[j - k])
+            basis_func[k] = cache + right[k + 1] * tmp
+            cache = left[j - k] * tmp
+        basis_func[j] = cache
+    return basis_func
+
+
+def get_cartesian_points(h_pnts):
+    shape = list(h_pnts.shape)
+    h_coords = h_pnts.reshape(-1, shape[-1]).transpose()
+    coords = np.divide(h_coords[:-1], h_coords[-1])
+    shape[-1] -= 1
+    return coords.transpose().reshape(*shape)
+
+
+def get_homogeneous_points(pnts, weights):
+    shape = list(pnts.shape)
+    coords = pnts.reshape(-1, shape[-1]).transpose()
+    weights = weights.reshape(-1)
+    h_coords = np.vstack((np.multiply(coords, weights), weights))
+    shape[-1] += 1
+    return h_coords.transpose().reshape(*shape)
+
+
 def intersect_lines(pos_v, dir_v):
     p13 = pos_v[0] - pos_v[1]
     d1343 = np.dot(p13, dir_v[1])
