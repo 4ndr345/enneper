3f7ed678b0ac7a3fc5a54ae977c8914609704853
Andreas Kührmann <andreas.kuehrmann@gmail.com>
2013-09-06 15:36:13 +0200

    ADDED: initialiser and properties to Surface class

---
+++ src/enneper/surface.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ***************************************************************************
+# *   Copyright (C) 2013 by Andreas Kührmann [andreas.kuehrmann@gmail.com]  *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program; if not, write to the                         *
+# *   Free Software Foundation, Inc.,                                       *
+# *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+# ***************************************************************************
+
+
+class Surface(object):
+
+    def __init__(self):
+        """designated initializer"""
+        self._ctrl_pnts = None
+        self._knots_u = None
+        self._knots_u = None
+        self._deg_u = None
+        self._deg_v = None
+
+##############################################################################
+# properties
+##############################################################################
+
+    @property
+    def ctrl_pnts(self):
+        return self._ctrl_pnts
+
+    @property
+    def knots_u(self):
+        return self._knots_u
+
+    @property
+    def knots_v(self):
+        return self._knots_v
+
+    @property
+    def deg_u(self):
+        return self._deg_u
+
+    @property
+    def deg_v(self):
+        return self._deg_v
